e50d2aeea0f166555cdf4e2489199740965b0051
David Smit <david.d.smit@gmail.com>
2020-12-07 22:38:20 -0500

    day7p1

--- day7/day7.py
+++ day7/day7.py
@@ -5,33 +5,15 @@ def get_file(file_path: str):
         inputs = file.readlines()
     return [line.strip('\n') for line in inputs if line]
 
-def find_bag(rules, bag="shiny yellow", counter=0):
-    rules = rules.copy()
-    for key, bags in rules.items():
-        print(key,bags)
-        for temp_bag in bags:
-            if bag.lower() in temp_bag.lower():
-                counter += 1
-            elif temp_bag in rules:
-                print(temp_bag)
-                counter += find_bag({temp_bag:rules[temp_bag]}, bag, counter)
-        # return counter
-    if counter != 0:
-        x = 1
-    return counter
 
-def find_bag(rules_containing_bag, bag='shiny yellow', counter = 0):
-    for containing_bag, rule in rules_containing_bag.items():
-        print(bag, containing_bag)
-        if bag == containing_bag:
-            counter += 1
+def find_bag(rules, bag='shiny gold', collection = None):
+    if not collection:
+        collection = set()
+    for containing_bag, rule in rules.items():
         if bag in rule:
-            print('bag found')
-            counter += 1
-            return find_bag(rules, containing_bag, counter)
-    return counter
-
-
+            collection.add(containing_bag)
+            collection = find_bag(rules, containing_bag, collection)
+    return collection
 
 data = get_file('d7.txt')
 
@@ -44,8 +26,9 @@ rules = dict()
 for key, value in rules_raw.items():
     contained_items = {
         item.strip()[item.strip().find(' ')+1:].replace(' bags','').replace(' bag',' ').replace('.','').strip():
-            (item.strip()[:item.strip().find(' ')])
+            int((item.strip()[:item.strip().find(' ')]))
         for item in value.split(',')
+        if not 'no' in item
     }
     rules.update(
         {
@@ -53,12 +36,5 @@ for key, value in rules_raw.items():
         }
     )
 
-rules_containing_bag = {
-    key:value
-    for key, value in rules.items()
-    if 'shiny yellow' in value
-}
-print(rules_containing_bag)
-
-print('start')
-print(find_bag(rules))
+print(rules)
+print(len(find_bag(rules)))
